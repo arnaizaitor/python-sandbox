93f27d359c6dd6836fb55c25aa9312011d430172
Aitor Arnaiz <aitorarnaiz@MBP-de-Aitor.home>
2021-05-06 00:42:03 +0200

    rendering templates

--- flask-learn/app.py
+++ flask-learn/app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
+###################### G L O B A L   V A R I A B L E S ######################
+posts = ['Alpha', 'Beta', 'Gamma', 'Delta']
+
+#############################   R O U T I N G   #############################
 @app.route('/')
 def home():
-    return 'Hello world'
+    return render_template("index.html", posts=posts)  
+
+@app.route('/post/<string:slug>/')
+def show_post_slug(slug):
+    return render_template("post_view.html", slug_title=slug)
+
+@app.route("/admin/post/")
+@app.route("/admin/post/<int:post_id>/")
+def post_form(post_id=None):
+    return render_template("admin/post_form.html", post_id=post_id)
 
 if __name__ == "__main__":
     app.run()
