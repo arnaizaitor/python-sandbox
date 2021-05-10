f0bb0d925a172e56bc0b4a37cf222b7e74d6cd88
Aitor Arnaiz <aitorarnaiz@MBP-de-Aitor.home>
2021-05-10 23:18:30 +0200

    added index pagination

--- flask-learn/app.py
+++ flask-learn/app.py
@@ -5,10 +5,30 @@ from data.data import posts
 
 app = Flask(__name__, template_folder='./frontend/templates',static_folder='./frontend/static')
 
+class PageResult:
+   def __init__(self, data, page = 0, posts_per_page = 32):
+    self.__dict__ = dict(zip(['data', 'page', 'number'], [data, page, posts_per_page]))
+    self.posts = data[page*posts_per_page:(page+1)*posts_per_page]
+    self.isLastPage = False
+    if(len(self.posts) < posts_per_page):
+        self.isLastPage = True
+    self.isVoidNextPage = False
+    if(len(data[(page+1)*posts_per_page:(page+2)*posts_per_page]) == 0):
+        self.isVoidNextPage = True
+    
+   def __iter__(self):
+     for i in self.posts:
+       yield i
+
+def getPostsInPage(data, pagenum, posts_per_page):
+    return data[pagenum*posts_per_page:(pagenum+1)*posts_per_page]
+
+
 #############################   R O U T I N G   #############################
 @app.route('/')
-def home():
-    return render_template("index.html", posts=posts)  
+@app.route('/<int:pagenum>')
+def home(pagenum = 0):
+    return render_template("index.html", posts = PageResult(posts, pagenum))  
 
 @app.route('/post/<string:slug>/')
 def show_post_slug(slug):
