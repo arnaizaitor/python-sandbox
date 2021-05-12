c7a36c85304408ffdebcb4c172d6b4fa68b700b9
Aitor Arnaiz <aitorarnaiz@MBP-de-Aitor.home>
2021-05-12 02:58:38 +0200

    connected to mongodb

--- flask-learn/app.py
+++ flask-learn/app.py
@@ -1,38 +1,45 @@
+import sys
+
 from flask import Flask
 from flask import render_template
-
-from data.data import posts
+#PyMongo
+from flask_pymongo import PyMongo
+# PageResult 
+from frontend.templates.PageResult import PageResult 
+# BDD
+from data.data import posts, post_body
 
 app = Flask(__name__, template_folder='./frontend/templates',static_folder='./frontend/static')
+# DB config
+app.config["MONGO_URI"] = "mongodb://localhost:27017/flaskMongoDB"
+mongo = PyMongo(app, connect=True)
 
-class PageResult:
-   def __init__(self, data, page = 0, posts_per_page = 32):
-    self.__dict__ = dict(zip(['data', 'page', 'number'], [data, page, posts_per_page]))
-    self.posts = data[page*posts_per_page:(page+1)*posts_per_page]
-    self.isLastPage = False
-    if(len(self.posts) < posts_per_page):
-        self.isLastPage = True
-    self.isVoidNextPage = False
-    if(len(data[(page+1)*posts_per_page:(page+2)*posts_per_page]) == 0):
-        self.isVoidNextPage = True
-    
-   def __iter__(self):
-     for i in self.posts:
-       yield i
-
-def getPostsInPage(data, pagenum, posts_per_page):
-    return data[pagenum*posts_per_page:(pagenum+1)*posts_per_page]
+#try
+'''
+i = 0
+for post in posts:
+    mongo.db.posts.insert({
+        "id" : i,
+        "title" : post,
+        "content" : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
+    })
+    i = i+1
+'''
+posts_mdb = list(mongo.db.posts.find({}))
+postList = [post['title'] for post in posts_mdb]
 
 
 #############################   R O U T I N G   #############################
 @app.route('/')
-@app.route('/<int:pagenum>')
+@app.route('/posts')
+@app.route('/posts/<int:pagenum>')
 def home(pagenum = 0):
-    return render_template("index.html", posts = PageResult(posts, pagenum))  
+    print(list(postList), file=sys.stderr)
+    return render_template("index.html", posts = PageResult(postList, pagenum))  
 
 @app.route('/post/<string:slug>/')
 def show_post_slug(slug):
-    return render_template("post_view.html", slug_title=slug)
+    return render_template("post_view.html", slug_title = slug, post_body = post_body)
 
 @app.route("/admin/post/")
 @app.route("/admin/post/<int:post_id>/")
---
+++ flask-learn/frontend/templates/PageResult.py
@@ -0,0 +1,14 @@
+class PageResult:
+   def __init__(self, data, page = 0, posts_per_page = 32):
+    self.__dict__ = dict(zip(['data', 'page', 'number'], [data, page, posts_per_page]))
+    self.posts = data[page*posts_per_page:(page+1)*posts_per_page]
+    self.isLastPage = False
+    if(len(self.posts) < posts_per_page):
+        self.isLastPage = True
+    self.isVoidNextPage = False
+    if(len(data[(page+1)*posts_per_page:(page+2)*posts_per_page]) == 0):
+        self.isVoidNextPage = True
+    
+   def __iter__(self):
+     for i in self.posts:
+       yield i
